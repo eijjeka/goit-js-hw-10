93dd7a83de932685dbb9b841b56801ad773c9f2c
eijjeka <eijjeka@gmail.com>
2022-01-28 11:43:03 +0200

    add files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries';
+import heandleCountry from './country.hbs';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+    input: document.querySelector('#search-box')
+}
+
+console.log(fetchCountries('ukraine'))
